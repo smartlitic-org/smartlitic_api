2d5e9367ddec0ad6f7a5d2870bd5fc70417eba18
Khashayar <khashayar.rajaee@gmail.com>
2022-09-01 18:39:26 +0430

    Add development staticfiles url patterns

--- smartlitic_api/urls.py
+++ smartlitic_api/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
