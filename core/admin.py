34e95d360b67501cbe8ebdb8b77ca6cdc6480e7b
Khashayar <khashayar.rajaee@gmail.com>
2022-09-02 15:21:15 +0430

    Add  and  models' admin

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from .models import User
+from .models import User, APIKey, Project
 
 
 @admin.register(User)
@@ -23,3 +23,16 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('-id',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'project_uuid', 'user', 'slug', 'name', 'is_enable']
+    prepopulated_fields = {'slug': ['name']}
+    search_fields = ['project_uuid', 'slug']
+
+
+@admin.register(APIKey)
+class APIKeyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'api_key', 'is_revoked']
+    search_fields = ['api_key']
