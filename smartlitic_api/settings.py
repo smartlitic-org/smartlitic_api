addc55ba266ad501e1fbc76c7b014a0fd3fcf9c3
Khashayar <khashayar.rajaee@gmail.com>
2022-10-12 00:32:25 +0330

    Add jwt auth integration

--- smartlitic_api/settings.py
+++ smartlitic_api/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
 
     'core.apps.CoreConfig',
--- smartlitic_api/urls.py
+++ smartlitic_api/urls.py
@@ -3,10 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('logger/', include('core.urls')),
+
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
 
 if settings.DEBUG:
