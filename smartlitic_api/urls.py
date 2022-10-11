369343ab61d144326be7fb773977a5e677a90790
Khashayar <khashayar.rajaee@gmail.com>
2022-10-12 02:16:09 +0330

    Move token related apis into `users` django app

--- smartlitic_api/urls.py
+++ smartlitic_api/urls.py
@@ -3,18 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('logger/', include('logger.urls')),
-
-    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
 
 if settings.DEBUG:
