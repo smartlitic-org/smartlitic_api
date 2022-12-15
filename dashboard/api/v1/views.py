6d5f05d4afaa10dc0089fce052b4e9940a5231ad
Khashayar <khashayar.rajaee@gmail.com>
2022-12-15 19:45:01 +0330

    Add empty days and hours in audience_overview chart data

--- dashboard/api/v1/views.py
+++ dashboard/api/v1/views.py
@@ -1,5 +1,10 @@
 import datetime
 
+from collections import OrderedDict
+from dateutil.parser import parse
+
+from django.utils import timezone
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -42,6 +47,22 @@ class DashboardGeneralView(APIView):
     serializer_class = DashboardGeneralSerializer
 
     @staticmethod
+    def convert_dict_to_chart_data(chart_dict_data, datetime_sort=False):
+        parser_func = int
+        if datetime_sort:
+            parser_func = parse
+
+        sorted_data = OrderedDict(sorted(chart_dict_data.items(), key=lambda x: parser_func(x[0])))
+        chart_data = {
+            'labels': [],
+            'data': [],
+        }
+        for key_name, value in sorted_data.items():
+            chart_data['labels'].append(key_name)
+            chart_data['data'].append(value)
+        return chart_data
+
+    @staticmethod
     def generate_search_query(user_id, project_id, index_name, start_time, end_time, event_type, log_type):
         search_query = Search(using=elasticsearch_connector.get_connection(), index=index_name)
         search_query = search_query.filter('term', user_id=user_id)
@@ -52,10 +73,10 @@ class DashboardGeneralView(APIView):
         return search_query
 
     @staticmethod
-    def generate_chart_data(elasticsearch_search_query):
+    def generate_chart_data(elasticsearch_search_query, dict_response=False):
         chart_data = {
             'labels': [],
-            'data': []
+            'data': [],
         }
         try:
             result = elasticsearch_search_query.execute()
@@ -67,7 +88,14 @@ class DashboardGeneralView(APIView):
             else:
                 chart_data['labels'].append(bucket.key)
             chart_data['data'].append(bucket.doc_count)
-        return chart_data
+
+        if not dict_response:
+            return chart_data
+
+        chart_dict_data = {}
+        for index in range(len(chart_data['data'])):
+            chart_dict_data[chart_data['labels'][index]] = chart_data['data'][index]
+        return chart_dict_data
 
     def create_session_device_chart(self, user_id, project_id, index_name, start_time, end_time):
         search_query = self.generate_search_query(user_id, project_id, index_name,
@@ -96,7 +124,29 @@ class DashboardGeneralView(APIView):
 
         group_by_day = A('date_histogram', **aggregate_params)
         search_query.aggs.bucket('group_by', group_by_day)
-        return self.generate_chart_data(search_query)
+
+        chart_dict_data: dict = self.generate_chart_data(search_query, dict_response=True)
+        convert_kwargs = {}
+
+        if report_type == 'today':
+            now_hour = timezone.now().hour
+            for hour in range(0, now_hour + 1):
+                hour = str(hour)
+                if hour not in chart_dict_data.keys():
+                    chart_dict_data[hour] = 0
+        else:
+            convert_kwargs.update(datetime_sort=True)
+            generated_times = []
+            tmp_time = start_time
+            while tmp_time <= end_time:
+                generated_times.append(tmp_time)
+                tmp_time += datetime.timedelta(days=1)
+            for generated_time in generated_times:
+                generated_time = str(generated_time)
+                if generated_time not in chart_dict_data.keys():
+                    chart_dict_data[generated_time] = 0
+
+        return self.convert_dict_to_chart_data(chart_dict_data, **convert_kwargs)
 
     def get(self, request, project_id):
         serializer = self.serializer_class(data=request.query_params)
@@ -104,7 +154,7 @@ class DashboardGeneralView(APIView):
 
         report_type = serializer.validated_data['report_type']
         if report_type == 'today':
-            start_time = datetime.datetime.now().date()
+            start_time = timezone.now().date()
             end_time = start_time + datetime.timedelta(days=1)
         else:
             start_time = serializer.validated_data['report_from']
