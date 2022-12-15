a9f5cecddb17864461e9344d37bcb0f8f06de967
Khashayar <khashayar.rajaee@gmail.com>
2022-12-15 17:17:45 +0330

    Add `create_audience_overview_chart` in Dashboard General API

--- dashboard/api/v1/views.py
+++ dashboard/api/v1/views.py
@@ -42,33 +42,68 @@ class DashboardGeneralView(APIView):
     serializer_class = DashboardGeneralSerializer
 
     @staticmethod
-    def create_session_device_chart(user_id, project_id, index_name, start_time, end_time):
+    def generate_search_query(user_id, project_id, index_name, start_time, end_time, event_type, log_type):
         search_query = Search(using=elasticsearch_connector.get_connection(), index=index_name)
         search_query = search_query.filter('term', user_id=user_id)
         search_query = search_query.filter('term', project_id=project_id)
-        search_query = search_query.filter('term', event_type='LOAD_COMPLETE')
-        search_query = search_query.filter('term', log_type='GENERAL')
+        search_query = search_query.filter('term', event_type=event_type)
+        search_query = search_query.filter('term', log_type=log_type)
         search_query = search_query.filter('range', created_time={'gte': start_time, 'lt': end_time})
+        return search_query
 
-        client_device_types = A('terms', field='client_device_type')
-        search_query.aggs.bucket('client_device_types', client_device_types)
-
-        chart_data = {'labels': [], 'data': []}
+    @staticmethod
+    def generate_chart_data(elasticsearch_search_query):
+        chart_data = {
+            'labels': [],
+            'data': []
+        }
         try:
-            result = search_query.execute()
+            result = elasticsearch_search_query.execute()
         except NotFoundError:
             return chart_data
-        for bucket in result.aggregations.client_device_types.buckets:
-            chart_data['labels'].append(bucket.key)
+        for bucket in result.aggregations.group_by.buckets:
+            if hasattr(bucket, 'key_as_string'):
+                chart_data['labels'].append(bucket.key_as_string)
+            else:
+                chart_data['labels'].append(bucket.key)
             chart_data['data'].append(bucket.doc_count)
-
         return chart_data
+
+    def create_session_device_chart(self, user_id, project_id, index_name, start_time, end_time):
+        search_query = self.generate_search_query(user_id, project_id, index_name,
+                                                  start_time, end_time, 'LOAD_COMPLETE', 'GENERAL')
+        client_device_types = A('terms', field='client_device_type')
+        search_query.aggs.bucket('group_by', client_device_types)
+        return self.generate_chart_data(search_query)
+
+    def create_audience_overview_chart(self, user_id, project_id, index_name, report_type, start_time, end_time):
+        search_query = self.generate_search_query(user_id, project_id, index_name,
+                                                  start_time, end_time, 'LOAD_COMPLETE', 'GENERAL')
+        if report_type == 'today':
+            aggregate_params = {
+                'field': 'created_time',
+                'time_zone': 'Asia/Tehran',
+                'interval': 'hour',
+                'format': 'k',
+            }
+        else:
+            aggregate_params = {
+                'field': 'created_time',
+                'time_zone': 'Asia/Tehran',
+                'interval': 'day',
+                'format': 'yyy-MM-dd',
+            }
+
+        group_by_day = A('date_histogram', **aggregate_params)
+        search_query.aggs.bucket('group_by', group_by_day)
+        return self.generate_chart_data(search_query)
 
     def get(self, request, project_id):
         serializer = self.serializer_class(data=request.query_params)
         serializer.is_valid(raise_exception=True)
 
-        if serializer.validated_data['report_type'] == 'today':
+        report_type = serializer.validated_data['report_type']
+        if report_type == 'today':
             start_time = datetime.datetime.now().date()
             end_time = start_time + datetime.timedelta(days=1)
         else:
@@ -81,5 +116,8 @@ class DashboardGeneralView(APIView):
             'sessions_device': self.create_session_device_chart(
                 user_id, project_id, logger_index, start_time, end_time
             ),
+            'audience_overview': self.create_audience_overview_chart(
+                user_id, project_id, logger_index, report_type, start_time, end_time
+            )
         }
         return Response(result)
